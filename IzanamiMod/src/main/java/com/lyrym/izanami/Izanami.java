13875e78440f448847d025a26aa813cffff86dfc
Lyrym <mathis.tit@gmail.com>
2022-05-20 10:25:14 +0200

    Damage event

--- IzanamiMod/src/main/java/com/lyrym/izanami/Izanami.java
+++ IzanamiMod/src/main/java/com/lyrym/izanami/Izanami.java
@@ -18,9 +18,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("izanami")
+
+
+@Mod(Izanami.MOD_ID)
 public class Izanami
 {
+    public static final String MOD_ID = "izanami";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
