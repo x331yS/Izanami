ee8882214e488aff3de5b67afbf4c7c7dda365db
FLEJOSNE <florian.lejosne@ynov.com>
2022-05-24 10:27:52 +0200

    Website

---
+++ Website/action.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$color = $_POST['color'];
+list($r, $g, $b) = sscanf($color, "#%02x%02x%02x");
+
+
+include '../../../config.php';
+$conn = new mysqli("$servername", "$username", "$password", "$dbname");
+echo $dbname;
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE currentprofile SET profile = 'WEBSITE', r=$r, g=$g, b=$b";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+header("Location: index.html");
+?>
